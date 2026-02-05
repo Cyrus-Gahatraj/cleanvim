aaeb30ce33b78edb6d6badc16717445bfdec5655
Cyrus-Gahatraj <cyrusgahatraj301@gmail.com>
2026-02-05 09:25:58 +0545

    fix: theme issue

--- lua/cleanvim/ui/theme.lua
+++ lua/cleanvim/ui/theme.lua
@@ -1,15 +1,19 @@
 local M = {}
 
 M.setup = function()
-    local theme_name = vim.g.cleanvim_theme
+    local theme_name = vim.g.cleanvim_theme or "catppuccin"
     local loader_ok, loader = pcall(require, "lazy.core.loader")
-    if loader_ok then
-        pcall(loader.load, { theme_name }, { reason = "UI Config" })
+	local config_ok, config = pcall(require, "lazy.core.config")
+
+    if loader_ok and config_ok then
+		if config.plugins[theme_name] then
+			pcall(loader.load, { theme_name }, { reason = "UI Config" })
+		end
     end
 
     local colors_ok, _ = pcall(vim.cmd.colorscheme, theme_name)
     if not colors_ok then
-        vim.cmd.colorscheme("state")
+        vim.cmd.colorscheme("default")
         print("Cleanvim: Theme '" .. theme_name .. "' not found, using fallback.")
     end
 end
