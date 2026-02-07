fb0d7d7447404a9a3c785a9a3381d457f0a3d672
Cyrus-Gahatraj <cyrusgahatraj301@gmail.com>
2026-02-07 21:04:39 +0545

    changed undo tree

--- lua/cleanvim/plugins/undotree.lua
+++ lua/cleanvim/plugins/undotree.lua
@@ -1,17 +1,9 @@
 return {
-	"mbbill/undotree",
-	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {
-			desc = "Undo Tree"
-		})
-		if vim.fn.has("persistent_undo") == 1 then
-            local target_path = vim.fn.expand("~/.undodir")
-            if vim.fn.isdirectory(target_path) == 0 then
-                vim.fn.mkdir(target_path, "p")
-            end
-
-            vim.opt.undodir = target_path
-            vim.opt.undofile = true
-        end
-	end
+  "jiaoshijie/undotree",
+  opts = {},
+   config = function()
+	  vim.keymap.set("n", "<leader>u", require('undotree').toggle, {
+		  desc = "Undo tree"
+	  })
+  end
 }
