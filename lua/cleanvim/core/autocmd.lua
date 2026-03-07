cf6ddea0dc9ea282a0d2e3a1b82dbaa8c00d2f3d
Cyrus-Gahatraj <cyrusgahatraj301@gmail.com>
2026-03-07 16:55:50 +0545

    auto start tree-sitter

--- lua/cleanvim/core/autocmd.lua
+++ lua/cleanvim/core/autocmd.lua
@@ -30,3 +30,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end
 })
+
+-- Auto start tree-sitter
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        pcall(vim.treesitter.start) end,
+})
+
